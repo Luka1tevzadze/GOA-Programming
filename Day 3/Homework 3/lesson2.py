82997cd768410db16ac14f95893e2e745a887c59
Luka1tevzadze <lukatevzadze43@gmail.com>
2023-07-18 16:15:53 +0400

    fourth commit

--- Day 3/Homework 3/lesson2.py
+++ Day 3/Homework 3/lesson2.py
@@ -3,13 +3,9 @@ surname = "Tevzadze"
 age = "Is 10 Years Old"
 height = "and is 50.5 cm"
 country = "and lives in Georgia"
-City = "and lives in a city called Tbilisi"
+city = "and lives in a city called Tbilisi"
 
 knows_programming = True
 is_ugly = False
 
-if knows_programming := True:
-    print("Hello!")
-
-
-print(name, surname, age, height, country, City,)
+print(name, surname, age, height, country, city,)
---
+++ Day 4/Homework 1/time traveler nika!/time_travel_nika.py
@@ -0,0 +1,4 @@
+name = "Nika"
+age = 15
+
+print(15+3.5)
