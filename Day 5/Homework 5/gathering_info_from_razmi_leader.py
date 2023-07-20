21088acaffdc70a934f136ac6aafc020ffca0cbd
Luka1tevzadze <lukatevzadze43@gmail.com>
2023-07-20 23:36:46 +0400

    fith commit

---
+++ Day 5/Homework 5/gathering_info_from_razmi_leader.py
@@ -0,0 +1,10 @@
+name = input("What is your name? :") 
+surname = input("and. and what is your surname? :")
+age = input("your age? :")                                   # Starting off Questions
+what_food_like = input("What food do you like? :")
+if_likes_goa = input("Do you like GOA? :")
+
+
+# Printing Out the inputs...
+
+print(name + " " + surname + " " + "is" + " " + str(age) + " " + "years old" + " " + "and likes" + " " + what_food_like + " " + "and about GOA" + " " + "said" + " " + if_likes_goa)
