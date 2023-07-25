c4ed32c1cb0ce31289aceea1accb6474cc5c9d29
Luka1tevzadze <lukatevzadze43@gmail.com>
2023-07-25 12:38:08 +0400

    sixth commit

---
+++ Day 6/Homework 6/find_out_parents_age_is_50.py
@@ -0,0 +1,3 @@
+father_age = input("Your father's age? :") # Asks the user for there fathers age.
+
+print(int(father_age) > 60) # Prints out false if father is not older than 60,  Or else prints out true if the father is older than 60
---
+++ Day 6/Homework 6/is_age_18_or_not.py
@@ -0,0 +1,6 @@
+print("Age limit is 18!")
+
+age = input("How, old are you? :")
+
+
+print(int(age) > 18)
