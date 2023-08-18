70ad4880f23b5c2f1343de1bd95212100e64dc03
Luka Tevzadze <lukatevzadze43@gmail.com>
2023-08-17 21:42:35 -0700

    12th commit
    12th commit

---
+++ Day 12/Homework 12/homework2.py
@@ -0,0 +1,5 @@
+name = "luk"
+family_member_1 = "eka"
+family_member_2 = "ten"
+
+print(name[0] + name[1] + family_member_1[1] + family_member_1[2] + " " + family_member_1 + family_member_2[0] + family_member_2[1] + name[0])
---
+++ Day 12/Homework 12/vowel.py
@@ -0,0 +1,4 @@
+name = "luka"
+surname = "tevzadze"
+
+print(name[3] + " " + surname[1] + " i" + " o" + " u")
