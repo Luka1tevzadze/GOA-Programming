30f9b6a8776edfac70fe7fa0275b8c061af27bfe
Luka Tevzadze <lukatevzadze43@gmail.com>
2023-08-29 09:28:32 -0700

    15th commit
    15th commit

---
+++ Day 15/Homework 15/my_favourite_list.py
@@ -0,0 +1,7 @@
+fav_book = "Rich Dad Poor Dad"
+fav_country = "Georgia"
+
+print(fav_book[0:4] + " " + fav_book[5:8])
+print(fav_book[8:12])
+print(fav_country[0:3])
+print(fav_country[4:7])
---
+++ Day 15/Homework 15/saqartvelo_slicing.py
@@ -0,0 +1,7 @@
+country = "Saqartvelo"
+
+print(country[2:10]) # output: Qartvelo
+print(country[1:3]) # output: aq
+print(country[1:4]) # output: aqa
+print(country[3:5] + country[1]) # output: ar + a
+print(country[6:10]) # output: velo
